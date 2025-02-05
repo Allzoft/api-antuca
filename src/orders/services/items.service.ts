aa8d6ca33f4cb98e8625899411a74dddbb25ed9f
Rodrigo Zelaya <120606993+Zoloz966@users.noreply.github.com>
2025-02-05 10:28:08 -0400

    fix(item service): add logic with restaurant column

--- src/orders/services/items.service.ts
+++ src/orders/services/items.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -13,19 +17,25 @@ export class ItemsService {
   constructor(
     @InjectRepository(Items)
     private itemRepository: Repository<Items>,
-    
-     private userContextService: UserContextService,
+
+    private userContextService: UserContextService,
   ) {}
 
   async create(createItemDto: CreateItemDto) {
+    const restaurantId =
+      this.userContextService.getUser().restaurantIdRestaurant;
     const newItem = this.itemRepository.create(createItemDto);
+
+    newItem.restaurantIdRestaurant = restaurantId;
 
     return await this.itemRepository.save(newItem);
   }
 
   async findAll() {
+    const restaurantId =
+      this.userContextService.getUser().restaurantIdRestaurant;
     const list = await this.itemRepository.find({
-      where: { status: 1 },
+      where: { status: 1, restaurantIdRestaurant: restaurantId },
     });
     if (!list.length) {
       throw new NotFoundException({ message: 'Empty list' });
@@ -37,11 +47,12 @@ export class ItemsService {
 
     return list;
   }
-  
 
   async findOne(id: number) {
+    const restaurantId =
+      this.userContextService.getUser().restaurantIdRestaurant;
     const item = await this.itemRepository.findOne({
-      where: { id_item: id, status: 1 },
+      where: { id_item: id, restaurantIdRestaurant: restaurantId, status: 1 },
     });
     if (!item) {
       throw new NotFoundException(`This item #${id} not found`);
@@ -50,20 +61,37 @@ export class ItemsService {
   }
 
   async update(id: number, updateItemDto: UpdateItemDto) {
+    const restaurantId =
+      this.userContextService.getUser().restaurantIdRestaurant;
     const item = await this.itemRepository.findOneBy({ id_item: id });
 
     this.itemRepository.merge(item, updateItemDto);
+
+    if (item.restaurantIdRestaurant !== restaurantId) {
+      throw new UnauthorizedException(
+        `This item #${id} not belong to your restaurant`,
+      );
+    }
 
     return this.itemRepository.save(item);
   }
 
   async remove(id: number) {
+    const restaurantId =
+      this.userContextService.getUser().restaurantIdRestaurant;
     const item = await this.itemRepository.findOneBy({ id_item: id });
     const deleteItem: UpdateItemDto = {
       status: 0,
     };
 
     this.itemRepository.merge(item, deleteItem);
+
+    if (item.restaurantIdRestaurant !== restaurantId) {
+      throw new UnauthorizedException(
+        `This item #${id} not belong to your restaurant`,
+      );
+    }
+
     return this.itemRepository.save(item);
   }
 }
