30b0d73851d29a5d55e3376fe832daa56dc24d13
Rodrigo Zelaya <120606993+Zoloz966@users.noreply.github.com>
2025-02-05 17:35:25 -0400

    feature/add-report-interfaces

---
+++ src/reports/interfaces/dailyReport.ts
@@ -0,0 +1,10 @@
+export interface DailyReport {
+    total_menus: number;
+    weeklyGrowthRate_menus: number
+    total_incomes: number;
+    weeklyGrowthRate_incomes: number;
+    total_orders: number;
+    weeklyGrowthRate_orders: number;
+    total_customers: number;
+    weeklyGrowthRate_customers: number;
+}
---
+++ src/reports/interfaces/orderSumaryByServiceMode.ts
@@ -0,0 +1,6 @@
+export interface OrderSumaryByMode {
+  total_orders: number;
+  orders_by_delivery: number;
+  orders_by_pickup: number;
+  orders_by_table: number;
+}
