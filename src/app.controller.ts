0d715dee039c7b543eb1474983e26f8f55c2bba4
Rodrigo Zelaya <120606993+Zoloz966@users.noreply.github.com>
2025-02-11 12:52:57 -0400

    we add guard to upload files!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,13 @@ import {
   Logger,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 import { diskStorage } from 'multer';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -18,13 +20,14 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
         destination: './uploads',
         filename: (req, file, callback) => {
-          const sufix = 'img' + Date.now();
+          const sufix = 'file' + Date.now();
           const filename = `${sufix}-${file.originalname}`;
           callback(null, filename);
         },
